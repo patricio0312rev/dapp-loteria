09c567ca5444de5063a33889abfe5669f85d6ffa
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-22 11:41:19 -0500

    Componentes e imagenes importadas

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
 import './App.css';
+import { Container } from 'semantic-ui-react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css'
+import { Header } from './Header';
+import { Tokens } from './Tokens';
+import { Loteria } from './Loteria';
+import { Premios } from './Premios';
 
 class App extends Component {
-  
-  
+  render(){
+    return(
+      <BrowserRouter>
+        <Container>
+          <Header />
+          
+          <main>
+            <Switch>
+              <Route exact path="/" Component={Tokens}/>
+              <Route exact path="/loteria" Component={Loteria}/>
+              <Route exact path="/premios" Component={Premios}/>
+            </Switch>
+          </main>
+        </Container>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
